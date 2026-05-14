b94679a38431b35ecb9b1878d79e7bed9b1dc21e
SkyyV24 <abarac@frba.edu.utn.ar>
2026-05-14 01:32:43 -0300

    Faltan tests

--- README.md
+++ README.md
@@ -11,13 +11,12 @@
 
 ## Nombre y legajo
 
-- Legajo: **Completar** 
-- Nombre: **Completar**
+- Legajo: 232.927-0
+- Nombre: Nicolas Agustín Barac
 
 ## El enunciado
 
-
-Resolver este TP  **usando composición y aplicación parcial**. Escribir todos los tests que consideren necesarios para cada punto.
+Resolver este TP **usando composición y aplicación parcial**. Escribir todos los tests que consideren necesarios para cada punto.
 
 ### Parte 1: Hamburguesas
 
@@ -33,30 +32,33 @@ El precio final de una hamburguesa es la sumatoria de los precios de los ingredi
 - Curry = 5
 - QuesoDeAlmendras = 15
 - BaconDeTofu = 12
- 
+
 Un ejemplo de hamburguesa es el **cuartoDeLibra**, una hamburguesa de pan, carne, cheddar, pan y su precio base es de 20.
 
- Partiendo de esto modelamos:
- 
+Partiendo de esto modelamos:
+
 - **agrandar**: cada vez que se agranda una hamburguesa se agrega otro ingrediente base (por ahora, son Carne o Pollo), se elige el ingrediente base a agregar según lo que ya haya en la hamburguesa (si había carne se agrega carne, si había pollo se agrega pollo, si había ambos da igual cuál se agregue).
 - **agregarIngrediente**: recibe un ingrediente y una hambrugesa lo agrega a la hamburguesa.
 - **descuento**: recibe un % de descuento, y devuelve la hamburguesa con ese descuento aplicado al precio base.
 - la **pdepBurger**, que es un cuarto de libra agrandado 2 veces con panceta, cheddar y 20% de descuento. Su precio final deberia ser 110.
 
 ### PARTE 2: Algunas hamburguesas más
+
 El negocio se agrandó y queremos agregar las siguientes hamburguesas:
+
 - **dobleCuarto** = es un cuarto de libra con carne y cheddar. El precio final deberia ser 84.
 - **bigPdep** = es un doble cuarto con curry. El precio final deberia ser 89.
 - **delDia** = es una promo que, dada una hamburguesa, le agrega Papas y un descuento del 30%. Por ejemplo, podría pedir una big pdep del dia y debería ser como una big pdep (doble cuarto con curry) pero con papas y el descuento del 30%. Por ejemplo una doble cuarto del día deberia valer 88.
 
 Las papas son un ingrediente que cuesta $10.
 
-### PARTE 3: algunos cambios más 
+### PARTE 3: algunos cambios más
 
 Queremos modelar los siguientes modificadores:
+
 - **hacerVeggie** : cambia todos los ingredientes base que hayan en la hamburguesa por PatiVegano (nuevo ingrediente base, también de precio 10), el cheddar lo cambia por queso de almendras y la panceta por bacon de tofu.
 
   _Nota: ahora que hay un nuevo ingrediente base, **agrandar** una hamburguesa debería modificarse de manera acorde. Es decir, agrandar una hamburguesa cuyo ingrediente base es un PatiVegano debería agregarle otro PatiVegano._
+
 - **cambiarPanDePati** : cambia el Pan que haya en la hamburguesa por PanIntegral (ingrediente de precio 3).
 - hacer el **dobleCuartoVegano**, que es un dobleCuarto veggie con pan integral.
-
--- src/Library.hs
+++ src/Library.hs
@@ -1,20 +1,87 @@
 module Library where
+
+import GHC.IO.Handle.Types (Handle__ (haBufferMode))
 import PdePreludat
 
-data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
-    deriving (Eq, Show)
+data Ingrediente = Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | BaconDeTofu | Papas | PatiVegano | PanIntegral
+  deriving (Eq, Show)
 
 precioIngrediente Carne = 20
 precioIngrediente Pan = 2
 precioIngrediente Panceta = 10
 precioIngrediente Cheddar = 10
-precioIngrediente Pollo =  10
+precioIngrediente Pollo = 10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente BaconDeTofu = 12
+precioIngrediente Papas = 10
+precioIngrediente PatiVegano = 10
+precioIngrediente PanIntegral = 3
 
-data Hamburguesa = Hamburguesa {
-    precioBase :: Number,
+data Hamburguesa = Hamburguesa
+  { precioBase :: Number,
     ingredientes :: [Ingrediente]
-} deriving (Eq, Show)
+  }
+  deriving (Eq, Show)
 
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa 20 [Pan, Carne, Cheddar, Pan]
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente ingrediente hamburguesa = hamburguesa {ingredientes = ingrediente : ingredientes hamburguesa}
+
+agregarVarios :: [Ingrediente] -> Hamburguesa -> Hamburguesa
+agregarVarios [] hamburguesa = hamburguesa
+agregarVarios (x : xs) hamburguesa = agregarVarios xs (agregarIngrediente x hamburguesa)
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar hamburguesa
+  | (elem Carne . ingredientes) hamburguesa = agregarIngrediente Carne hamburguesa
+  | (elem Pollo . ingredientes) hamburguesa = agregarIngrediente Pollo hamburguesa
+  | (elem PatiVegano . ingredientes) hamburguesa = agregarIngrediente PatiVegano hamburguesa
+  | otherwise = hamburguesa
+
+aplicarDescuento :: Number -> Number -> Number
+aplicarDescuento porcentaje precio = precio * (1 - porcentaje)
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento porcentaje hamburguesa = hamburguesa {precioBase = aplicarDescuento porcentaje (precioBase hamburguesa)}
+
+pdepBurger :: Hamburguesa
+pdepBurger = descuento 0.2 . agregarVarios [Panceta, Cheddar] . agrandar . agrandar $ cuartoDeLibra
+
+-- Parte 2 ---
+
+dobleCuarto :: Hamburguesa
+dobleCuarto = agregarVarios [Carne, Cheddar] cuartoDeLibra
+
+bigPdep :: Hamburguesa
+bigPdep = agregarIngrediente Curry dobleCuarto
+
+delDia :: Hamburguesa -> Hamburguesa
+delDia = descuento 0.3 . agregarIngrediente Papas
+
+-- PARTE 3 --
+
+transformarIngrediente :: Ingrediente -> Ingrediente
+transformarIngrediente Carne = PatiVegano
+transformarIngrediente Pollo = PatiVegano
+transformarIngrediente Cheddar = QuesoDeAlmendras
+transformarIngrediente Panceta = BaconDeTofu
+transformarIngrediente Pan = PanIntegral
+transformarIngrediente otro = otro
+
+hacerVeggie :: Hamburguesa -> Hamburguesa
+hacerVeggie hamburguesa =
+  hamburguesa
+    { ingredientes = map transformarIngrediente (ingredientes hamburguesa)
+    }
+
+cambiarPanDePati :: Hamburguesa -> Hamburguesa
+cambiarPanDePati hamburguesa =
+  hamburguesa
+    { ingredientes = map transformarIngrediente (ingredientes hamburguesa)
+    }
+
+dobleCuartoVegano :: Hamburguesa
+dobleCuartoVegano = hacerVeggie . cambiarPanDePati $ dobleCuarto
